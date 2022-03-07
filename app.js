7213276e526f3c9a8e7aa47d95086517b48c75db
Meet Vyas <meet.vyas@somaiya.edu>
2022-03-08 00:24:18 +0530

    Fixed hours bug

--- app.js
+++ app.js
@@ -29,16 +29,18 @@ const setDateTime = _ => {
     const dayNames = ["1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th", "11th", "12th", "13th", "14th", "15th", "16th", "17th", "18th", "19th", "20th", "21st", "22nd", "23rd", "24th", "25th", "26th", "27th", "28th", "29th", "30th", "31st"]
 
     const clock = document.querySelector(".clockarea__date-time");
+    let hours = parseInt(time.getHours()) < 10 ? `0${time.getHours()}` : time.getHours();
     let mins = parseInt(time.getMinutes()) < 10 ? `0${time.getMinutes()}` : time.getMinutes();
-    clock.textContent = `${time.getHours()}:${mins}`;
+    clock.textContent = `${hours}:${mins}`;
 
     const dateDay = document.querySelector(".clockarea__date")
     dateDay.textContent = `${dayNames[time.getDay()]} ${monthNames[time.getMonth()]} ${time.getFullYear()}`
 
     const updateTime = setInterval(() => {
         time = new Date;
+        hours = parseInt(time.getHours()) < 10 ? `0${time.getHours()}` : time.getHours();
         mins = parseInt(time.getMinutes()) < 10 ? `0${time.getMinutes()}` : time.getMinutes();
-        clock.textContent = `${time.getHours()}:${mins}`
+        clock.textContent = `${hours}:${mins}`
     }, 1000);
 
 }
